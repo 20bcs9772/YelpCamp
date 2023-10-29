0a13d79ae6a76a7538cab186268bb7fd2652517e
Madhav <20bcs9772@cuchd.in>
2023-10-29 21:57:15 +0530

    added seasons in campground schema and ui

--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -1,88 +1,248 @@
 module.exports.descriptors = [
-    'Forest',
-    'Ancient',
-    'Petrified',
-    'Roaring',
-    'Cascade',
-    'Tumbling',
-    'Silent',
-    'Redwood',
-    'Bullfrog',
-    'Maple',
-    'Misty',
-    'Elk',
-    'Grizzly',
-    'Ocean',
-    'Sea',
-    'Sky',
-    'Dusty',
-    'Diamond',
-    'Sunken',
-    'Wild',
-    'Crimson'
-]
+  "Forest",
+  "Ancient",
+  "Petrified",
+  "Roaring",
+  "Cascade",
+  "Tumbling",
+  "Silent",
+  "Redwood",
+  "Bullfrog",
+  "Maple",
+  "Misty",
+  "Elk",
+  "Grizzly",
+  "Ocean",
+  "Sea",
+  "Sky",
+  "Dusty",
+  "Diamond",
+  "Sunken",
+  "Wild",
+  "Crimson",
+];
 
 module.exports.places = [
-    'Flats',
-    'Village',
-    'Canyon',
-    'Pond',
-    'Group Camp',
-    'Horse Camp',
-    'Ghost Town',
-    'Camp',
-    'Dispersed Camp',
-    'Backcountry',
-    'River',
-    'Creek',
-    'Creekside',
-    'Bay',
-    'Spring',
-    'Bayshore',
-    'Sands',
-    'Mule Camp',
-    'Hunting Camp',
-    'Cliffs',
-    'Hollow'
-]
+  "Flats",
+  "Village",
+  "Canyon",
+  "Pond",
+  "Group Camp",
+  "Horse Camp",
+  "Ghost Town",
+  "Camp",
+  "Dispersed Camp",
+  "Backcountry",
+  "River",
+  "Creek",
+  "Creekside",
+  "Bay",
+  "Spring",
+  "Bayshore",
+  "Sands",
+  "Mule Camp",
+  "Hunting Camp",
+  "Cliffs",
+  "Hollow",
+];
 
 module.exports.pics = [
-    'nature',
-    'tree',
-    'forest',
-    'mountain',
-    'water',
-    'cloudy',
-    'camp',
-    'river',
-    'canyon',
-    'cliffs'
-]
+  "nature",
+  "tree",
+  "forest",
+  "mountain",
+  "water",
+  "cloudy",
+  "camp",
+  "river",
+  "canyon",
+  "cliffs",
+];
 
 module.exports.morePics = [
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628083318/photo-1547140741-00d6fd251528_rgrauk.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628083280/photo-1525113990976-399835c43838_p6ewyw.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628083246/photo-1506974210756-8e1b8985d348_l7v2gz.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628083188/photo-1456030680012-9aa5bd962cc4_po9tpv.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628083152/photo-1516214104703-d870798883c5_bmipjk.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628083117/photo-1473448912268-2022ce9509d8_tuyz0g.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628083088/photo-1503265192943-9d7eea6fc77a_pmktfq.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082994/photo-1565964249053-7f30b7cdba09_ytr9cw.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082963/photo-1517419455681-74fdb65819c4_hflw3s.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082909/photo-1517174871454-c03ec956f05e_b1dhlt.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082871/photo-1466220549276-aef9ce186540_xw6ji7.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082830/photo-1475483768296-6163e08872a1_dgsqmi.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082777/photo-1525811902-f2342640856e_akjnyu.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082722/photo-1471115853179-bb1d604434e0_jv0v5k.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082665/pexels-h-emre-2776479_mktrf3.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082602/pexels-kaboompics-com-5969_zk4max.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082552/pexels-francesco-ungaro-1671324_idbxqb.jpg",
-    "https://res.cloudinary.com/eclipse5137/image/upload/v1628082472/pexels-eberhard-grossgasteiger-1062249_lc3mms.jpg"
-
-]
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628083318/photo-1547140741-00d6fd251528_rgrauk.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628083280/photo-1525113990976-399835c43838_p6ewyw.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628083246/photo-1506974210756-8e1b8985d348_l7v2gz.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628083188/photo-1456030680012-9aa5bd962cc4_po9tpv.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628083152/photo-1516214104703-d870798883c5_bmipjk.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628083117/photo-1473448912268-2022ce9509d8_tuyz0g.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628083088/photo-1503265192943-9d7eea6fc77a_pmktfq.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082994/photo-1565964249053-7f30b7cdba09_ytr9cw.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082963/photo-1517419455681-74fdb65819c4_hflw3s.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082909/photo-1517174871454-c03ec956f05e_b1dhlt.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082871/photo-1466220549276-aef9ce186540_xw6ji7.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082830/photo-1475483768296-6163e08872a1_dgsqmi.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082777/photo-1525811902-f2342640856e_akjnyu.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082722/photo-1471115853179-bb1d604434e0_jv0v5k.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082665/pexels-h-emre-2776479_mktrf3.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082602/pexels-kaboompics-com-5969_zk4max.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082552/pexels-francesco-ungaro-1671324_idbxqb.jpg",
+  "https://res.cloudinary.com/eclipse5137/image/upload/v1628082472/pexels-eberhard-grossgasteiger-1062249_lc3mms.jpg",
+];
 
 module.exports.users = [
-    "653d49b0a37360588cec82dd",
-    "653d4c373d68601420920192",
-    "653d4ca1fe2c5228c8adcdd4"
-]
+  "653d49b0a37360588cec82dd",
+  "653d4c373d68601420920192",
+  "653d4ca1fe2c5228c8adcdd4",
+];
+
+module.exports.countries = [
+  {
+    name: "USA",
+    states: [
+      {
+        name: "California",
+        cities: [
+          { name: "Los Angeles", longitude: -118.248779, latitude: 34.052235 },
+          {
+            name: "San Francisco",
+            longitude: -122.419418,
+            latitude: 37.774929,
+          },
+        ],
+      },
+      {
+        name: "New York",
+        cities: [
+          { name: "New York City", longitude: -74.006058, latitude: 40.712776 },
+          { name: "Buffalo", longitude: -78.878372, latitude: 42.886444 },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Canada",
+    states: [
+      {
+        name: "Ontario",
+        cities: [
+          { name: "Toronto", longitude: -79.383186, latitude: 43.653225 },
+          { name: "Ottawa", longitude: -75.697197, latitude: 45.421528 },
+        ],
+      },
+      {
+        name: "British Columbia",
+        cities: [
+          { name: "Vancouver", longitude: -123.121643, latitude: 49.28273 },
+          { name: "Victoria", longitude: -123.365644, latitude: 48.428421 },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Brazil",
+    states: [
+      {
+        name: "São Paulo",
+        cities: [
+          { name: "São Paulo", longitude: -46.633309, latitude: -23.55052 },
+          {
+            name: "Rio de Janeiro",
+            longitude: -43.172896,
+            latitude: -22.906847,
+          },
+        ],
+      },
+      {
+        name: "Bahia",
+        cities: [
+          { name: "Salvador", longitude: -38.501381, latitude: -12.973291 },
+          {
+            name: "Feira de Santana",
+            longitude: -38.955654,
+            latitude: -12.266951,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Germany",
+    states: [
+      {
+        name: "Berlin",
+        cities: [{ name: "Berlin", longitude: 13.404954, latitude: 52.520007 }],
+      },
+      {
+        name: "Bavaria",
+        cities: [
+          { name: "Munich", longitude: 11.576124, latitude: 48.137154 },
+          { name: "Nuremberg", longitude: 11.074822, latitude: 49.452102 },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Japan",
+    states: [
+      {
+        name: "Tokyo",
+        cities: [
+          { name: "Tokyo", longitude: 139.691711, latitude: 35.689487 },
+          { name: "Yokohama", longitude: 139.642502, latitude: 35.443708 },
+        ],
+      },
+      {
+        name: "Osaka",
+        cities: [
+          { name: "Osaka", longitude: 135.502197, latitude: 34.693737 },
+          { name: "Kobe", longitude: 135.196652, latitude: 34.690083 },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Australia",
+    states: [
+      {
+        name: "New South Wales",
+        cities: [
+          { name: "Sydney", longitude: 151.20929, latitude: -33.86882 },
+          { name: "Newcastle", longitude: 151.785046, latitude: -32.928272 },
+        ],
+      },
+      {
+        name: "Victoria",
+        cities: [
+          { name: "Melbourne", longitude: 144.963158, latitude: -37.813629 },
+          { name: "Geelong", longitude: 144.35096, latitude: -38.147286 },
+        ],
+      },
+    ],
+  },
+  {
+    name: "South Africa",
+    states: [
+      {
+        name: "Gauteng",
+        cities: [
+          { name: "Johannesburg", longitude: 28.024345, latitude: -26.204103 },
+          { name: "Pretoria", longitude: 28.229271, latitude: -25.747868 },
+        ],
+      },
+      {
+        name: "Western Cape",
+        cities: [
+          { name: "Cape Town", longitude: 18.424055, latitude: -33.924869 },
+          { name: "Stellenbosch", longitude: 18.873969, latitude: -33.932104 },
+        ],
+      },
+    ],
+  },
+];
+
+module.exports.sample = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+module.exports.words = () => {
+    let para = " ";
+    for (let k = 0; k < 120; k++) {
+      para =
+        (Math.random() + 1)
+          .toString(36)
+          .substring(Math.floor(Math.random() * 4) + 7) +
+        " " +
+        para;
+    }
+    return para;
+  };
